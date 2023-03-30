92b9a5b3defc968700f2e761cc233aee81ac3a92
Sergiy Sechko <ssechko94@gmail.com>
2023-03-30 16:52:49 +0300

    add 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery')
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+
+galleryBox.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createImgCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="large-image.jpg">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>`
+        
+    })
+        .join("");
+}
+console.log(createImgCardsMarkup(galleryItems));
+
+galleryBox.addEventListener('click', clickOnImg);
+
+function clickOnImg (event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => {
+        window.addEventListener('keydown', onKeydownEsc);
+      },
+      onClose: () => {
+        window.removeEventListener('keydown', onKeydownEsc);
+      },
+        },
+    );
+
+    function onKeydownEsc(event) {
+        if (event.code === 'Escape') {
+            instance.close()
+        }
+    }
+    
+  
+    
+    
+
+    instance.show();   
+}
