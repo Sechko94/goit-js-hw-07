afbf28ccf54ceec7a8e4d39dcd7826637e14ebd7
Sergiy Sechko <ssechko94@gmail.com>
2023-03-30 17:39:31 +0300

    add 02-lightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
+const cardsMarkup = createImgCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createImgCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </div>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    caption: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    enableKeyboard: true,
+
+});
